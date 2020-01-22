a70b0f4cf022d48d7da550bc098be32a4cec75a3
MacStudent <macstudent@128-IMAC27.local>
2020-01-21 20:48:46 -0500

    [Monika]Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+
+public class FirstExample
+{
+    public static void main(String[] args)
+    {
+       System.out.println("Welcome to Java ");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Monika Sharma");
+        }
+
+    }
+}
