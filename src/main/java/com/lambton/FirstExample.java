93bb00b9c86231678353231106925649a39d6b20
Monika Sharma <monikasharma26081991@gmail.com>
2020-01-22 18:46:42 -0500

    Day@ coding datatYpe work

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -12,11 +12,46 @@ public class FirstExample
         }
        //sample commit -1
 
-        while(int a=0)
-        {
-            System.out.println("test");
-
-        }
         //Second Commit -2
+
+        //Day 2  Code starts
+        int a= 100;
+        int b;
+        b=200;
+        int c =a+b;
+
+
+        System.out.println("Sum ="+c);
+        System.out.println(a+b + "sum");
+
+
+        System.out.println("Short max=" + Short.MAX_VALUE);
+        System.out.println("Short min=" + Short.MIN_VALUE);
+
+        long y=500;
+        System.out.println(y);
+        System.out.println("long max=" + Long.MAX_VALUE);
+        System.out.println("long min=" + Long.MIN_VALUE);
+
+        float f1 = 1000.66f;
+        double d = 323232.34d;
+                  int atest= 10/3;
+                  float te=10/3;
+        float tea=  10.00f/3;
+        int tea1= (int)(10.0f/3);
+        float t6=(float)10/3;
+System.out.println(t6);
+        System.out.println("Float f1=" +tea);
+        System.out.println("Float f1=" +tea1);
+
+
+        System.out.println("int" +atest);
+        System.out.println("Float f1=" +te);
+
+        int x2=1_00_5_0;
+        System.out.println(x2);
+
+
+
     }
 }
